b7e742398143b3d3248bf1c2b262e3d99ed7b8b2
p1345lebg <p1345lebg@gmail.com>
2025-04-27 16:28:49 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const articles = document.getElementById("articles");
+
+const load_articles = () => {
+    fetch('datas/articles.json')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('erreur réseau')
+            }
+            return response.json();
+        })
+        .then(data => {
+            var articlesTempo = ""
+            data.forEach((element,index) => {
+                articlesTempo += `
+                    <div class="article" id="article${index}">
+                        <h2>${element.title}</h2>
+                        <p>${element.content}</p>
+                        <p>${element.autor}</p>
+                    </div>
+                `;
+            });
+            articles.innerHTML = articlesTempo;
+        });
+}
+
+
+load_articles();
