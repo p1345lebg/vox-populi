d860f88a3a943a035a2f6c5fb5e8594c2ab01355
felar59 <felar59000@gmail.com>
2025-05-04 15:37:47 +0200

    design de création de news

--- pages/createNews.js
+++ pages/createNews.js
@@ -1,28 +1,16 @@
 console.log('ok')
 
-const load_create_article = () => {
-    const main = document.getElementById("main");
-    main.innerHTML = `
-        <div class"creerArticle">
-            <h1>creer article</h1>
-            <input type="text" class="PlaceholderNameArticle" id="articleName">nom de l'article</input>
-            <textarea id="articleContent"></textarea>
-            <input type="date" class="PlaceholderDateArticle" id="articleDate">date de l'article</input>
-            <input type="text" class="PlaceholderAutorArticle" id="articleAutor">auteur de l'article</input>
-            <button class="submit" onclick="create_article()">soumettre</button>
-        </div>
-    `
-}
-
 const create_article = () => {
     const title = document.getElementById("articleName").value;
     const image = null;
     const content = document.getElementById("articleContent").value;
     const autor = document.getElementById("articleAutor").value;
+    // country et city lié à l'utilisateur
     const contry = null;
     const city = null;
+
     const date = document.getElementById("articleDate").value;
-    const hour = null;
+    const hour = null; // obtenable avec js
 
     const article = {
         "title" : title,
