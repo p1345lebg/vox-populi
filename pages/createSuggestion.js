7d42e28ec4e6b05837e99573fca1f0e1d628feb6
p1345lebg <p1345lebg@gmail.com>
2025-05-05 17:53:06 +0200

    ajustements

---
+++ pages/createSuggestion.js
@@ -0,0 +1,52 @@
+console.log('ok')
+
+const load_create_article = () => {
+    const main = document.getElementById("main");
+    main.innerHTML = `
+        <div class"creerSuggestion">
+            <h1>soumetre une suggestio our une loi</h1>
+            <input type="text" class="PlaceholderNameSuggestion" id="suggestionName" placeholder="nom de la suggestion">nom de l'article</input>
+            <textarea id="suggestionContent" placeholder="contenu de la suggestion"></textarea>
+            <input type="date" class="PlaceholderDateSuggestion" id="suggestionContent">date de l'article</input>
+            <input type="text" class="PlaceholderAutorSuggestion" id="suggestionAutor" placeholder="auteur(s) de la suggestion"></input>
+            <button class="submit" onclick="create_suggestion()">soumettre</button>
+        </div>
+    `
+}
+
+const create_suggestion = () => {
+    const nom = document.getElementById("suggestionName").value;
+    const phraseAccroche = null
+    const details = document.getElementById("suggestionContent").value;
+    const autor = document.getElementById("suggestionAutor").value;
+    const textesConsernes = [];
+    const categories = [];
+
+    const suggestion = {
+        "nom" : "caca",
+        "phraseAccroche" : "on peut vendre notre caca",
+        "details" : "on peut vendre notre caca",
+        "textesConsernes" : ["DDHC"],
+        "categories" : ["economie", "ecologie"],
+        "autor" : "richard",
+        "avis" : {
+            "favorables" : [{
+                "nom" : "michel michel",
+                "avis" : "le caca c'est délicieux",
+                "nbNote" : 1,
+                "note" : 5
+                }
+            ],
+            "defavorables" : [{
+                "nom" : "jean michel",
+                "avis" : "oh nom cela vas a l'encontre de la vie pivée (tout le monde pourrais se procurer notre ADN)",
+                "nbNote" : 1,
+                "note" : 3
+                }
+            ]
+        }
+    }
+    console.log(suggestion)
+}
+
+load_create_article()
